66daa4964400277fc912113e97745a4a0eda948e
poipoiiii <3108410393@qq.com>
2023-12-07 20:15:25 +0800

    Site updated: 2023-12-07 20:15:23

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "A4C1F4",
+      "tmpSize": "16",
+      "cityColor": "A4C1F4",
+      "citySize": "16",
+      "aqiColor": "A4C1F4",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "zh",
+      "fixed": "true",
+      "vertical": "top",
+      "horizontal": "left",
+      "left": "550",
+      "top": "10",
+      "key": "e2726095affe42afbae40ca8fcd91ad1"
+    }
+  }
